df0d4f555d5d273dbf7ac19836d99785dbbcafcd
ismylravafidh <bd82v6x9y@code.edu.az>
2023-09-28 10:56:26 +0400

    Task1 and Task2

---
+++ task1.cs
@@ -0,0 +1,23 @@
+﻿int n = 128 ;
+int sum = 0;
+while (n >=2)
+{
+    if (n % 2 == 0)
+    {
+        sum ++;
+        n /= 2;
+    }
+    else
+    {
+        sum *= 0;
+        break;
+    }
+}
+if (sum == 0)
+{
+    Console.WriteLine("2-nin quvveti deyil");
+}
+else
+{
+    Console.WriteLine("2^" + sum);
+}
---
+++ task2.cs
@@ -0,0 +1,25 @@
+﻿int n = 30;
+int j=0;
+for(int i = 2; i <=n / 2; i++)
+{
+    if (n % i == 0)
+    {
+        j++;
+    }
+    else
+    {
+        j =0;
+    }
+}
+if (j != 0)
+{
+    Console.WriteLine("Murekkeb Ededdir");
+}
+else if (n == 1  || n==0)
+{
+    Console.WriteLine("Ne sade ne de murekkeb ededdir");
+}
+else
+{
+    Console.WriteLine("Sade Ededdir");
+}
